495e862598bad9461a8b64216267905d143aaa30
aliang1024 <wwl_23@qq.com>
2024-12-20 11:30:56 +0800

    yangshitiaozheng

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,25 +7,8 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      keyframes: {
-        'fade-up': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(10px)'
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)'
-          },
-        }
-      },
-      animation: {
-        'fade-up': 'fade-up 0.3s ease-out'
-      }
-    }
+    extend: {},
   },
   plugins: [],
 }
-
 export default config
